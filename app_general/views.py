4b3c8267767a29585fb6459f5bd19888456c250f
Jirayut Suansamran <jiiirayu2546@gmail.com>
2024-10-16 01:21:18 +0700

    Create more templates about subscription
    Create subscription_form and subscription_thankyou on app_general

--- app_general/views.py
+++ app_general/views.py
@@ -1,5 +1,6 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from app_rooms.models import Room
 
 # Create your views here.
 
@@ -8,3 +9,11 @@ def home(request):
 
 def about(request):
     return render(request, 'app_general/about.html')
+
+def subscription(request):
+    all_rooms = Room.objects.all()
+    context = {'rooms': all_rooms}
+    return render(request, 'app_general/subscription_form.html', context)
+
+def subscription_thankyou(request):
+    return render(request, 'app_general/subscription_thankyou.html')
