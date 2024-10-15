0c2b46fa590ba8f7cf6bffbdea3c009fa73686ab
Jirayut Suansamran <jiiirayu2546@gmail.com>
2024-10-15 22:47:15 +0700

    Add filters model on database
    Create some model on database in app_general file

--- app_general/models.py
+++ app_general/models.py
@@ -13,3 +13,4 @@ class Subscription(models.Model):
     email = models.EmailField(max_length=60, unique=True)
     status = models.CharField(max_length=15, choices=STATUS, default='unapproved')
     registered_at = models.DateTimeField(auto_now_add=True)
+    room = models.ForeignKey('app_rooms.Room', on_delete=models.SET_NULL, null=True)
