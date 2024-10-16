9a8d03bd2098cfecc3b23c0ff9c8e842e9a56287
Jirayut Suansamran <jiiirayu2546@gmail.com>
2024-10-16 15:04:02 +0700

    Create superuser (Admin)
    Create and add some data in admin on app_general and app_rooms

--- app_rooms/admin.py
+++ app_rooms/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from app_rooms.models import Room
 
 # Register your models here.
+
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price', 'is_multiple_bed', 'check_in_date', 'check_out_date']
+    search_fields = ['title']
+    list_filter = ['is_multiple_bed']
+    
+admin.site.register(Room, RoomAdmin)
