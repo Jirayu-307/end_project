3b01b7508a89bc73e797e63282f287e21579aca5
Jirayut Suansamran <jiiirayu2546@gmail.com>
2024-10-16 19:01:15 +0700

    Create static
    Create static and add image on app_general and app_rooms

--- app_rooms/models.py
+++ app_rooms/models.py
@@ -9,3 +9,4 @@ class Room(models.Model):
     check_in_date = models.DateTimeField(null=True, blank=True)
     check_out_date = models.DateTimeField(null=True, blank=True)
     description = models.TextField(null=True, blank=True)
+    image_relative_url = models.CharField(max_length=50, null=True, blank=True)
